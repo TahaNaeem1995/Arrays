790e5ecc77d7c1e20f8bf3a56d39df5d288fa59f
TahaNaeem1995 <160178535+TahaNaeem1995@users.noreply.github.com>
2024-06-25 15:53:39 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,17 @@
+let carsName = ["civic","coralla","Audi","BMW"]
+//              0       1       2      3  index number
+console.log(carsName);
+
+
+//carsName.push("mercides 2222")
+
+//console.log(carsName);
+
+//carsName.pop()
+//console.log(carsName);
+
+//carsName.unshift("Mehran")
+//console.log(carsName);
+
+carsName.shift()
+console.log(carsName);
